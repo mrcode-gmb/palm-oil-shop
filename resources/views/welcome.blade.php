03341e121495fe1ec83e968359425db92d2aab0b
Ahmad Isah <ahmadisah6293@gmail.com>
2025-11-04 09:20:18 +0100

    d

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 <body class="font-sans text-gray-900 antialiased">
 
