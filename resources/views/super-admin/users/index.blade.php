3b13b2781a894f77ef40608e493d1a06f68fcc9a
Ahmad Isah <ahmadisah6293@gmail.com>
2025-11-12 06:44:17 +0100

    update

--- resources/views/super-admin/users/index.blade.php
+++ resources/views/super-admin/users/index.blade.php
@@ -11,6 +11,8 @@
 
         <table class="min-w-full divide-y divide-gray-200">
             <thead class="bg-gray-50">
+                
+                
                 <tr>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Business</th>
@@ -18,6 +20,8 @@
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Joined</th>
                 </tr>
+
+                
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
                 @forelse($users as $user)
