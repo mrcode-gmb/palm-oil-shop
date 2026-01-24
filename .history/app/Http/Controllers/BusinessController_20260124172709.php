358cbc456ae54c7122f09288cc6dcd7ab2e36af6
Abubkar Ibrahim <abubakaribrahim30012@gmail.com>
2026-01-24 17:27:17 +0100

    making

--- .history/app/Http/Controllers/BusinessController_20260124172709.php
+++ .history/app/Http/Controllers/BusinessController_20260124172709.php
@@ -146,7 +146,7 @@ class BusinessController extends Controller
             $products = $assignment->assigned_quantity - $assignment->sold_quantity - $assignment->returned_quantity;
             return $products * $assignment->purchase->purchase_price;
         });
-        $productAssignmentQuantity = $business->productAssignments->sum(function ($assignment) {
+        $productAssignmentQuantity = $business->productAssignments->->sum(function ($assignment) {
             $products = $assignment->assigned_quantity - $assignment->sold_quantity - $assignment->returned_quantity;
             return $products;
         }) + $business->creditors->sum("amount");
