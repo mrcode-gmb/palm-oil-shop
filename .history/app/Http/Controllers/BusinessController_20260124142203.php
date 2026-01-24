942fb4381120c42f142e97a2fc4de35a96ea56c3
Abubkar Ibrahim <abubakaribrahim30012@gmail.com>
2026-01-24 14:22:33 +0100

    d

--- .history/app/Http/Controllers/BusinessController_20260124142203.php
+++ .history/app/Http/Controllers/BusinessController_20260124142203.php
@@ -229,7 +229,7 @@ class BusinessController extends Controller
     - ($historyPurchaseInventory ?? 0)
     - ($expenses ?? 0);
 
-        return number_format($actualWalletBalance, 2);
+        return number_format($balance, 2);
 
         // return $business->sales->sum(function($sale){
         //     return $sale->seller_profit_per_unit * $sale->quantity;
