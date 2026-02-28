21082780f83d6d729a77078c9b2c448a111caa12
Abubkar Ibrahim <abubakaribrahim30012@gmail.com>
2026-02-28 14:27:39 +0100

    paid credit

--- .history/app/Http/Controllers/BusinessController_20260228142732.php
+++ .history/app/Http/Controllers/BusinessController_20260228142732.php
@@ -130,7 +130,7 @@ class BusinessController extends Controller
             'total_credit_sales' => $business->sales->where("payment_type", "=", "credit")->sum(function($sale){
                 return $sale->selling_price_per_unit * $sale->quantity;
             }),
-            'total_profit' => $business->sales->where("payment_type", "!=", "credit")->sum('profit'),
+        'total_profit' => $business->sales->where("payment_type", "=", "credit")->sum('profit'),
             'total_profit_on_credit' => $business->sales->where("payment_type", "=", "credit")->sum('profit'),
             'total_quantity_sold' => $business->sales->sum('quantity'),
             'total_purchases' => $business->purchaseHistory->sum('total_cost'),
